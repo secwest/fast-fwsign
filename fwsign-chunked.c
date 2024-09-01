62396d20766022e7d74eb3f91097c02fe2bfe9e6
Secwest.net <70259601+secwest@users.noreply.github.com>
2024-08-31 19:00:03 -0700

    Update fwsign-chunked.c

--- fwsign-chunked.c
+++ fwsign-chunked.c
@@ -1,5 +1,5 @@
 /*
- * FAST-FWSIGN-CHUNKED   Firmware image encryption, signing and verification for embedded systems.
+ * FAST-FWSIGN-CHUNKED   Firmware image encryption, signing, and verification for embedded systems.
  *
  * BSD 3-Clause License
  * 
@@ -14,9 +14,9 @@
  * 2. Redistributions in binary form must reproduce the above copyright notice, 
  *    this list of conditions, and the following disclaimer in the documentation 
  *    and/or other materials provided with the distribution.
- * 3. Neither the name of Dragos nor the names of its contributors may be used 
- *    to endorse or promote products derived from this software without specific 
- *    prior written permission.
+ * 3. Neither the name of Dragos Ruiu, Dragostech.com Inc. nor the names of its 
+ *    contributors may be used to endorse or promote products derived from this 
+ *    software without specific prior written permission.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
